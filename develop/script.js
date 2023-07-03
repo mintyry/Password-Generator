1620c31d0defde4235096076292b095b3b7df4cf
mintyry <gitmintyry@gmail.com>
2023-07-03 15:53:44 -0700

    added vars and started function

--- develop/script.js
+++ develop/script.js
@@ -1,9 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatepassword() {
- 
+var uppset = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z'];
+var lowset = ['a', 'b', 'c', 'd', 'e', 'f', 'g,', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var numset= ['0', "1", '2', '3', '4', '5', '6', '7', '8', '9']
+var spset = ['!', '?', '$', '#', '&', '*', '@', '#', '(', ')']
+
+//var char = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//var passwordLength = 12;
+
+
+function generatePassword() {
+  var uppercase = confirm("Do you want uppercase letters in your password?");
   var lowercase = confirm("Do you want lowercase letters in your password?")//true or false
+  var numbers = confirm ("Do you want numbers in your password?")
+  var spchar = confirm ("Do you want special characters in your password?")
+  var passwordLength = prompt("Please enter a value between 8 and 128 for how many characters you would like your password to contain.")
+
+  if (uppercase+lowercase+numbers+spchar+passwordLength) {
+    var allset = [...uppset, ...lowset, ...numset, ...spset]
+    var randomset = allset[Math.floor(Math.random() * allset.length)];
+
+  }
+
+  for (let i=0; i <= passwordLength; i++) {
+  let randomset = allset[Math.floor(Math.random() * allset.length)];
+  }
  
   return password;
 }
